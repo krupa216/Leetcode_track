5b865f517f70912000bc67abb009c03cd738c665
krupa216 <krupahethsiyal@gmail.com>
2026-07-14 14:01:36 +0530

    Create 3476-FindMinimumOperationsToMakeAllElementsDivisibleByThree

---
+++ 3476-FindMinimumOperationsToMakeAllElementsDivisibleByThree/3476-FindMinimumOperationsToMakeAllElementsDivisibleByThree.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:01:36 PM
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%3!=0)
+            {
+               c++;
+            }
+
+        }
+        return c;
+    }
+}

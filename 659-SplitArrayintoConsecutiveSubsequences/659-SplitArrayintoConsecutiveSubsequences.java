a4cc89d77ed13fe0c3888f23f2cc7fa5bf061df0
krupa216 <krupahethsiyal@gmail.com>
2026-08-10 11:42:25 +0530

    File updated at 8/10/2026, 11:42:25 AM

--- 659-SplitArrayintoConsecutiveSubsequences/659-SplitArrayintoConsecutiveSubsequences.java
+++ 659-SplitArrayintoConsecutiveSubsequences/659-SplitArrayintoConsecutiveSubsequences.java
@@ -1,17 +1,20 @@
-// Last updated: 8/10/2026, 11:32:38 AM
+// Last updated: 8/10/2026, 11:42:25 AM
 1class Solution {
-2    public int smallestRangeII(int[] nums, int k) {
-3        Arrays.sort(nums);
-4        int r=nums[nums.length-1]-nums[0];
-5        int min=nums[0]+k;
-6        int max=nums[nums.length-1]-k;
-7        for(int i=0;i<nums.length-1;i++)
-8        {
-9           
-10            int max1=Math.max(max,nums[i]+k);
-11           int min2=Math.min(min,nums[i+1]-k);
-12           r=Math.min(r,max1-min2);
-13        }
-14        return r;
-15    }
-16}
+2    public int partitionDisjoint(int[] a) {
+3      int max=a[0];int ind=0;int smax=max;
+4      for(int i=0;i<a.length;i++)
+5      {
+6      if(max>a[i])
+7      {
+8        max=smax;
+9        ind=i;
+10
+11      }
+12      else
+13      {
+14        smax=Math.max(smax,a[i]);
+15      }
+16      }
+17        return ind+1;
+18    }
+19}

ac047a14c3f15ec5b19e1b069f3371a15e0433ba
krupa216 <krupahethsiyal@gmail.com>
2026-08-10 11:32:39 +0530

    File updated at 8/10/2026, 11:32:38 AM

--- 659-SplitArrayintoConsecutiveSubsequences/659-SplitArrayintoConsecutiveSubsequences.java
+++ 659-SplitArrayintoConsecutiveSubsequences/659-SplitArrayintoConsecutiveSubsequences.java
@@ -1,14 +1,17 @@
-// Last updated: 8/10/2026, 11:21:27 AM
+// Last updated: 8/10/2026, 11:32:38 AM
 1class Solution {
 2    public int smallestRangeII(int[] nums, int k) {
 3        Arrays.sort(nums);
 4        int r=nums[nums.length-1]-nums[0];
-5        for(int i=0;i<nums.length-1;i++)
-6        {
-7           int min=Math.min(nums[0]+k,nums[i+1]-k);
-8           int max=Math.max(nums[i]+k,nums[nums.length-1]-k);
-9           r=Math.min(r,max-min);
-10        }
-11        return r;
-12    }
-13}
+5        int min=nums[0]+k;
+6        int max=nums[nums.length-1]-k;
+7        for(int i=0;i<nums.length-1;i++)
+8        {
+9           
+10            int max1=Math.max(max,nums[i]+k);
+11           int min2=Math.min(min,nums[i+1]-k);
+12           r=Math.min(r,max1-min2);
+13        }
+14        return r;
+15    }
+16}

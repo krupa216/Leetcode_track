ed879cb58503d8521a8861e6a7a947000bfa015a
krupa216 <krupahethsiyal@gmail.com>
2026-08-16 17:39:10 +0530

    File updated at 8/16/2026, 5:39:11 PM

--- 3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.java
+++ 3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.java
@@ -1,23 +1,25 @@
-// Last updated: 8/16/2026, 4:45:56 PM
+// Last updated: 8/16/2026, 5:39:11 PM
 1class Solution {
-2    public int maxProduct(int n) {
-3        int max=0;int smax=0;
-4        int m=n;
-5        while(m>0)
-6        {
-7            int d=m%10;
-8            if(d>max)
-9            {
-10                smax=max;
-11                max=d;
-12            }
-13       
-14           else if(d>smax)
-15            {
-16                smax=d;
+2    public long sumAndMultiply(int n) {
+3        if(n==0)
+4        {
+5            return 0;
+6        }
+7        long sum=0;
+8        StringBuilder p=new StringBuilder();
+9        while(n>0)
+10        {
+11            int d=n%10;
+12            sum+=d;
+13            if(d!=0)
+14            {
+15              p.append(d);
+16
 17            }
-18            m/=10;
+18            n/=10;
 19        }
-20        return max*smax;
-21    }
-22}
+20        long s=Integer.valueOf(p.reverse().toString());
+21        System.out.println(sum*s);
+22    return sum*s;
+23    }
+24}

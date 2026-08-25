beeec2554d117ddbbc7cffefe808327d4bc1f933
krupa216 <krupahethsiyal@gmail.com>
2026-08-25 20:01:58 +0530

    File updated at 8/25/2026, 8:01:58 PM

--- 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.java
+++ 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.java
@@ -1,13 +1,17 @@
-// Last updated: 8/25/2026, 7:54:01 PM
+// Last updated: 8/25/2026, 8:01:58 PM
 1class Solution {
-2    public int minimumDifference(int[] nums, int k) {
-3        int min=Integer.MAX_VALUE;
-4        Arrays.sort(nums);
-5        for(int i=0;i<=nums.length-k;i++)
+2    public int divisorSubstrings(int num, int k) {
+3        String s=String.valueOf(num);
+4        int c=0;
+5        for(int i=0;i<=s.length()-k;i++)
 6        {
-7          int d=nums[i+k-1]-nums[i];
-8          min=Math.min(min,d);
-9        }
-10        return min;
-11    }
-12}
+7            String db=s.substring(i,i+k);
+8            int n1=Integer.parseInt(db);
+9            if(n1!=0 && num%n1==0)
+10            {
+11   c++;
+12            }
+13        }
+14        return c;
+15    }
+16}

4c468445ccca72030dffd3bed521a36a9ac32178
krupa216 <krupahethsiyal@gmail.com>
2026-09-02 11:43:04 +0530

    File updated at 9/2/2026, 11:43:04 AM

--- 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.java
+++ 3875-ConstructUniformParityArrayI/3875-ConstructUniformParityArrayI.java
@@ -1,6 +1,19 @@
-// Last updated: 9/2/2026, 11:23:21 AM
+// Last updated: 9/2/2026, 11:43:04 AM
 1class Solution {
-2    public boolean uniformArray(int[] nums1) {
-3        return true;
-4    }
-5}
+2    public int maximumLengthSubstring(String s) {
+3        int i=0;int j=0;int max=0;
+4        int[] f=new int[26];
+5        while(j<s.length())
+6        {
+7            char c=s.charAt(j);
+8            f[c-'a']++;
+9            while(f[c-'a']>2)
+10            {
+11                f[s.charAt(i)-'a']--;
+12                i++;
+13            }
+14            j++;
+15            max=Math.max(max,j-i);
+16        }
+17        return max;    }
+18}

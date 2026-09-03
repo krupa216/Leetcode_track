58e2828e4fb265ba551cbaac236fe438ae6476a4
krupa216 <krupahethsiyal@gmail.com>
2026-09-03 21:41:37 +0530

    File updated at 9/3/2026, 9:41:37 PM

--- 541-ReverseStringII/541-ReverseStringII.java
+++ 541-ReverseStringII/541-ReverseStringII.java
@@ -1,17 +1,6 @@
-// Last updated: 9/3/2026, 9:39:10 PM
+// Last updated: 9/3/2026, 9:41:37 PM
 1class Solution {
-2    public String reverseWords(String s) {
-3        String[] st=s.split(" ");
-4        String t="";
-5        for(int i=0;i<st.length;i++)
-6        {
-7            StringBuilder sb=new StringBuilder(st[i]);
-8            t+=sb.reverse();
-9            if(i<st.length-1)
-10            {
-11                t+=" ";
-12            }
-13        }
-14        return t;
-15    }
-16}
+2    public String toLowerCase(String s) {
+3        return s.toLowerCase();
+4    }
+5}

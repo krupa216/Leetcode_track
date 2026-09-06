7c68c035dfc9311fedbcecb94f707a6916e45bbb
krupa216 <krupahethsiyal@gmail.com>
2026-09-06 16:23:41 +0530

    Create 3689-MaximumTotalSubarrayValueI

---
+++ 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
@@ -0,0 +1,12 @@
+// Last updated: 9/6/2026, 4:23:41 PM
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
+        }
+        return (long) k*(max-min);    }
+}

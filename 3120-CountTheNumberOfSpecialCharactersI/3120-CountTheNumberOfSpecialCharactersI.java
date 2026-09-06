aff3a6f03f97ec022c0ed2f5b84bfc7887a39358
krupa216 <krupahethsiyal@gmail.com>
2026-09-06 16:24:06 +0530

    Create 3120-CountTheNumberOfSpecialCharactersI

---
+++ 3120-CountTheNumberOfSpecialCharactersI/3120-CountTheNumberOfSpecialCharactersI.java
@@ -0,0 +1,28 @@
+// Last updated: 9/6/2026, 4:24:06 PM
+class Solution {
+    public int numberOfSpecialChars(String s) {
+        HashSet<Character> l=new HashSet<>();
+           HashSet<Character> u=new HashSet<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isLowerCase(ch))
+            {
+                l.add(ch);
+            }
+            else{
+                u.add(ch);
+            }
+
+        }
+        int a=0;
+        for(char ch:l)
+        {
+            if(u.contains(Character.toUpperCase(ch)))
+            {
+                a++;
+            }
+        }
+        return a;
+    }
+}

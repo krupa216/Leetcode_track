91b3999d511402ff2722dd4d830519a2fdb3339b
krupa216 <krupahethsiyal@gmail.com>
2026-09-07 14:36:05 +0530

    File updated at 9/7/2026, 2:36:04 PM

--- 164-MaximumGap/164-MaximumGap.java
+++ 164-MaximumGap/164-MaximumGap.java
@@ -1,17 +1,19 @@
-// Last updated: 9/7/2026, 2:11:00 PM
+// Last updated: 9/7/2026, 2:36:04 PM
 1class Solution {
-2    public int maximumGap(int[] nums) {
-3        Arrays.sort(nums);
-4        int j=1;
-5        int max=0;
-6        for(int i=0;i<nums.length-1;i++)
-7        {
-8          if(nums[j]-nums[i]>max)
-9          {
-10            max=nums[j]-nums[i];
-11          }
-12          j++;
-13        }
-14        return max;
-15    }
-16}
+2    public List<Integer> majorityElement(int[] nums) {
+3        List<Integer> l=new ArrayList<>();
+4        HashMap<Integer,Integer> h=new HashMap<>();
+5        for(int i=0;i<nums.length;i++)
+6        {
+7            h.put(nums[i],h.getOrDefault(nums[i],0)+1);
+8        }
+9        for(Map.Entry<Integer, Integer> j :h.entrySet())
+10        {
+11            if(j.getValue()>nums.length/3)
+12            {
+13                l.add(j.getKey());
+14            }
+15        }
+16        return l;
+17    }
+18}

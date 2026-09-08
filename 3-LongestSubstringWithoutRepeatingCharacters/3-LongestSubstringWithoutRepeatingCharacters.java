fb3a6731bb82c07097f39fac0c1002bd5c4a4ce2
krupa216 <krupahethsiyal@gmail.com>
2026-09-08 15:12:49 +0530

    File updated at 9/8/2026, 3:12:49 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -1,23 +1,24 @@
-// Last updated: 7/14/2026, 2:04:48 PM
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-      String l="";
-      String cu="";
-
-      for(int i=0;i<s.length();i++)
-      {
-        if(cu.contains(String.valueOf(s.charAt(i))))
-        {
-           int index=cu.indexOf(s.charAt(i));
-           cu=cu.substring(index+1);
-        }
-        cu=cu+s.charAt(i);
-        if(cu.length()>l.length())
-        {
-            l=cu;
-        }
-      }
-      int c=l.length();
-      return c;
-    }
-}
+// Last updated: 9/8/2026, 3:12:49 PM
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3         String l="";
+4      String cu="";
+5
+6      for(int i=0;i<s.length();i++)
+7      {
+8        if(cu.contains(String.valueOf(s.charAt(i))))
+9        {
+10           int index=cu.indexOf(s.charAt(i));
+11           cu=cu.substring(index+1);
+12        }
+13        cu=cu+s.charAt(i);
+14        if(cu.length()>l.length())
+15        {
+16            l=cu;
+17        }
+18      }
+19      int c=l.length();
+20      return c;
+21    
+22    }
+23}

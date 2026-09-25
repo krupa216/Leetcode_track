7d72cdaca08547e45aa76cbc4d82d66978ee5800
krupa216 <krupahethsiyal@gmail.com>
2026-09-25 16:05:05 +0530

    File updated at 9/25/2026, 4:05:04 PM

--- 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.java
+++ 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.java
@@ -1,11 +1,15 @@
-// Last updated: 7/14/2026, 2:03:41 PM
-class Solution {
-    public int titleToNumber(String c) {
-        long a=0;
-        for(int i=0;i<c.length();i++)
-        {
-            a=a*26+(c.charAt(i)-'A'+1);
-        }
-        return (int)a;
-    }
-}
+// Last updated: 9/25/2026, 4:05:04 PM
+1class Solution {
+2    public boolean isPowerOfTwo(int n) {
+3        boolean b=false;
+4        for(int i=0;Math.pow(2,i)<=n;i++)
+5        {
+6            if(Math.pow(2,i)==n)
+7            {
+8                return true;
+9                
+10            }
+11        }
+12        return b;
+13    }
+14}
